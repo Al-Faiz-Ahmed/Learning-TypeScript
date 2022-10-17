e354e1e54728ff0ad302c50a21839e07e5a1cf61
sheikhfaiz546 <sheikhfaiz546@gmail.com>
2022-10-17 21:12:10 +0500

    TypeScript Start

---
+++ Learning from Youtube/app.js
@@ -0,0 +1,55 @@
+"use strict";
+exports.__esModule = true;
+// let a = "Hello World";
+// console.log(a);
+// class App {
+//   test() {
+//     console.log("hello world again");
+//   }
+// }
+// let cls = new App();
+// cls.test();
+// let a:number = 10
+// console.log(a);
+// interface CarsTypes {
+//   name: string;
+//   engine: number;
+//   weight: string;
+//   hybrid: boolean;
+// };
+// let cars: CarsTypes = {
+//   name: "Toyota",
+//   engine: 2.0,
+//   weight: "500kg",
+//   hybrid: false,
+// };
+// console.log(cars);
+// let name:(number|string|boolean) = "faizan"
+// console.log(name);
+// interface SuficiesntType {
+//     name:string,
+//     age:number,
+//     user:()=>string
+// }
+// let Suficiesnt:SuficiesntType = {
+//     name:"faizan",
+//     age:10,
+//     user:function(){
+//         return this.name
+//     }
+// }
+// console.log(Suficiesnt.user());
+// function dtop(a: string, b?: number): string | number {
+//   return a + b;
+// }
+// console.log(dtop("212", 45));
+// class Pappu {
+//   userName = "faizan";
+//   rollNo = 45;
+//   information(userName:string) {
+//     this.userName = userName
+//     return this.userName + this.rollNo;
+//   }
+// }
+// let pappu1 = new Pappu();
+// console.log(pappu1.information('uzair'));
